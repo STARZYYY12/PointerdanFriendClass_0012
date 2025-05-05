ba217d39655125e4db814bd245e81f6a183ab0c3
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-05-05 11:03:18 +0700

    member of mahasiswa

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -10,12 +10,3 @@ class mahasiswa
         cout << "No Induk = " <<nim << endl;
     }
 };
-
-int main()
-{
-    mahasiswa *mhs = new mahasiswa{1};
-    mhs->nim =2;
-    mhs->showNim();
-    delete mhs;
-    return 0;
-}
